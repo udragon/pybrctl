f72b54580cbf2e6f2a99eb5ffe13457bf3834ced
Ido Nahshon <udragon@gmail.com>
2015-01-23 07:16:22 -0800

    README Markdown to ReStructuredText

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ from setuptools import setup, find_packages
 
 setup(
 	name = "pybrctl",
-	version = "0.1",
+	version = "0.1.1",
 	packages = find_packages(),
 	author = "Ido Nahshon",
 	author_email = "udragon@gmail.com",
 	description = "Python brctl wrapper",
-	license = "GPL",
+	license = "GPLv2",
 	keywords = "brctl, bridge-utils",
 	url = "https://github.com/udragon/pybrctl",
-	long_description = open("README.md").read(),
-    classifiers = ['License :: OSI Approved :: GNU General Public' +
+	long_description = open("README.rst").read(),
+    classifiers = ['License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
                    'Programming Language :: Python',
                    'Operating System :: POSIX',
                    'Programming Language :: Python :: 2.7'],
